4f5a98547cfe6df8e1f29b1b5d24ef1846d21057
abrahimladha <abrahimladha@protonmail.ch>
2016-12-28 13:29:32 -0500

    lmao

---
+++ linux_test1/Assets/random.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var onoff : boolean;
+function Start () {
+onoff = Random.value >= 0.5;
+}
+
+function Update () {
+	//onoff = Random.value >= 0.5;
+    /*if (toggle == true)
+        gameObject.active = true;
+    if (toggle == false)
+        gameObject.active = false;
+	*/
+	gameObject.SetActive(onoff);
+}
